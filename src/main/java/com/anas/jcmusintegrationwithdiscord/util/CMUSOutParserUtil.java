75748e6fe11c3ca340be723ac08766e05d57f281
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-02 19:33:03 +0200

    Fix parse cmus out error 😆

--- src/main/java/com/anas/jcmusintegrationwithdiscord/util/CMUSOutParserUtil.java
+++ src/main/java/com/anas/jcmusintegrationwithdiscord/util/CMUSOutParserUtil.java
@@ -10,21 +10,21 @@ public class CMUSOutParserUtil {
     private CMUSOutParserUtil() {
         throw new IllegalStateException("Utility class");
     }
+
     public static Map<Tag, String> parse(String out) {
         Map<Tag, String> map = new EnumMap<>(Tag.class);
         Tag key = null;
         String value = null;
         String[] lines = out.split("\n");
         for (String line : lines) {
-            String[] parts = line.split("\\s");
+            String[] parts = line.split("\\s+");
             if (parts.length >= 3) {
                 key = Tag.getTag(parts[1]);
-                if (!line.startsWith("tag ") || key == null) {
-                    continue; // skip non-tag lines and unknown tags
-                }
                 value = line.substring(line.indexOf(parts[2]));
             }
+            if (parts[0].equals("tag") && key != null) { // skip non-tag lines and unknown tags
                 map.put(key, value);
+            }
         }
         return map;
     }
