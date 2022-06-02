c0c2ce130c7d3b5dbbaa9a67813834338e36aa2a
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-02 18:45:07 +0200

    Improve Responce class ☺

--- src/main/java/com/anas/jcmusintegrationwithdiscord/shell/Responce.java
+++ src/main/java/com/anas/jcmusintegrationwithdiscord/shell/Responce.java
@@ -9,33 +9,33 @@ import java.io.InputStreamReader;
 import java.util.logging.Level;
 
 public class Responce {
-    private StringBuilder responce;
-    private boolean isError;
+    private final StringBuilder stringBuilder;
+    private final boolean isError;
 
     public Responce(Process process) {
-        init(process);
+        stringBuilder = new StringBuilder();
+        isError = process.exitValue() != 0;
+        setupString(process);
     }
 
-    private void init(Process process) {
-        isError = process.exitValue() != 0;
-        responce = new StringBuilder();
-
+    private void setupString(Process process) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
         try {
             while ((line = reader.readLine()) != null) {
-                responce.append(line).append("\n");
+                stringBuilder.append(line).append("\n");
             }
         } catch (IOException e) {
             DebugManager.getInstance().debug(e.getMessage(), Level.SEVERE);
         }
     }
 
-    public String getResponce() {
-        return responce.toString();
-    }
-
     public boolean isError() {
         return isError;
+    }
+
+    @Override
+    public String toString() {
+        return stringBuilder.toString();
     }
 }
