b9744042920c85168eec583060f14cf537490ae9
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-26 21:13:17 +0200

    Add th java doc to the ArgumentsProcessor class 💙
    and some improves💙

--- src/main/java/com/anas/cmusrpc/Main.java
+++ src/main/java/com/anas/cmusrpc/Main.java
@@ -17,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(final String... args) {
-        new ArgumentsProcessor(args).process(); // process arguments
+        new ArgumentsProcessor().process(args); // process arguments
         DebugManager.getInstance().debug("cmus-rpc v" + VERSION);
         discordController = new DiscordController("961407969986232380");
         start();
