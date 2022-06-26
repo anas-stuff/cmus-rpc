987627127ce0e93baef7394d1926a306829db688
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-26 20:52:35 +0200

    Add the java doc to the DebugManager class 🥰

--- src/main/java/com/anas/cmusrpc/DebugManager.java
+++ src/main/java/com/anas/cmusrpc/DebugManager.java
@@ -4,6 +4,9 @@ package com.anas.cmusrpc;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * It's a singleton class that provides a debug method that logs messages to the console
+ */
 public class DebugManager {
     private boolean enabled;
     private final Logger logger;
@@ -15,6 +18,11 @@ public class DebugManager {
        logger = Logger.getLogger("Logger");
     }
 
+    /**
+     * If the instance is null, create a new instance and return it. Otherwise, return the existing instance
+     *
+     * @return The instance of the DebugManager class.
+     */
     public static DebugManager getInstance() {
         if (instance == null) {
             instance = new DebugManager();
@@ -22,6 +30,11 @@ public class DebugManager {
         return instance;
     }
 
+    /**
+     * If the logger is enabled, log the message at the specified level, if level is not specified, it defaults to INFO
+     *
+     * @param message The message to be logged.
+     */
     public void debug(String message, Level... level) {
         if (enabled) {
             if (level.length == 0) {
@@ -32,6 +45,11 @@ public class DebugManager {
         }
     }
 
+    /**
+     * Enable and disable the debugger
+     *
+     * @param debug true/false - enables/disables the debug mode
+     */
     public void setDebug(boolean debug) {
         enabled = debug;
     }
