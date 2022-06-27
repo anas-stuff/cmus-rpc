4bd50d32ea9c5b443e0e8cfbce565ba44693845a
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-27 04:48:33 +0200

    Up version 🙃

--- src/main/java/com/anas/cmusrpc/Main.java
+++ src/main/java/com/anas/cmusrpc/Main.java
@@ -9,7 +9,7 @@ import com.anas.cmusrpc.teracke.CMUSTracker;
  * thread
  */
 public class Main {
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "2.0.0";
     private static DiscordController discordController;
 
     /**
